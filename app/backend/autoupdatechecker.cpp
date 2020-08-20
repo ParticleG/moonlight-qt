58e3c920837892bb3582225e876896c00f78a5e1
Particle_G <gzy1135989508@gmail.com>
2020-08-20 18:13:37 +0800

    QTBUG-80947 is fixed for real in Qt 5.15.1

--- app/backend/autoupdatechecker.cpp
+++ app/backend/autoupdatechecker.cpp
@@ -1,22 +1,18 @@
 #include "autoupdatechecker.h"
 
-#include <QNetworkReply>
-#include <QJsonDocument>
 #include <QJsonArray>
+#include <QJsonDocument>
 #include <QJsonObject>
+#include <QNetworkReply>
 
-AutoUpdateChecker::AutoUpdateChecker(QObject *parent) :
-    QObject(parent),
-    m_Nam(this)
-{
+AutoUpdateChecker::AutoUpdateChecker(QObject *parent) : QObject(parent), m_Nam(this) {
     // Never communicate over HTTP
     m_Nam.setStrictTransportSecurityEnabled(true);
 
     // Allow HTTP redirects
     m_Nam.setRedirectPolicy(QNetworkRequest::NoLessSafeRedirectPolicy);
 
-    connect(&m_Nam, SIGNAL(finished(QNetworkReply*)),
-            this, SLOT(handleUpdateCheckRequestFinished(QNetworkReply*)));
+    connect(&m_Nam, SIGNAL(finished(QNetworkReply *)), this, SLOT(handleUpdateCheckRequestFinished(QNetworkReply *)));
 
     QString currentVersion(VERSION_STR);
     qDebug() << "Current Moonlight version:" << currentVersion;
@@ -26,19 +22,15 @@ AutoUpdateChecker::AutoUpdateChecker(QObject *parent) :
     Q_ASSERT(m_CurrentVersionQuad.count() > 1);
 }
 
-void AutoUpdateChecker::start()
-{
-#if defined(Q_OS_WIN32) || defined(Q_OS_DARWIN) || defined(STEAM_LINK) || defined(APP_IMAGE) // Only run update checker on platforms without auto-update
-#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0) && !defined(QT_NO_BEARERMANAGEMENT)
-    // HACK: Set network accessibility to work around QTBUG-80947.
-    // Even though it was fixed in 5.14.2, it still breaks for users attempting to
-    // directly connect their computers without a router using APIPA and in some cases
-    // using OpenVPN with IPv6 enabled. https://github.com/moonlight-stream/moonlight-qt/issues/375
+void AutoUpdateChecker::start() {
+#if defined(Q_OS_WIN32) || defined(Q_OS_DARWIN) || defined(STEAM_LINK) || defined(APP_IMAGE)    // Only run update checker on platforms without auto-update
+#    if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0) && QT_VERSION < QT_VERSION_CHECK(5, 15, 1) && !defined(QT_NO_BEARERMANAGEMENT)
+    // HACK: Set network accessibility to work around QTBUG-80947 (introduced in Qt 5.14.0 and fixed in Qt 5.15.1)
     QT_WARNING_PUSH
     QT_WARNING_DISABLE_DEPRECATED
     m_Nam.setNetworkAccessible(QNetworkAccessManager::Accessible);
     QT_WARNING_POP
-#endif
+#    endif
 
     // We'll get a callback when this is finished
     QUrl url("https://moonlight-stream.org/updates/qt.json");
@@ -46,16 +38,12 @@ void AutoUpdateChecker::start()
 #endif
 }
 
-void AutoUpdateChecker::parseStringToVersionQuad(QString& string, QVector<int>& version)
-{
+void AutoUpdateChecker::parseStringToVersionQuad(QString &string, QVector<int> &version) {
     QStringList list = string.split('.');
-    for (const QString& component : list) {
-        version.append(component.toInt());
-    }
+    for (const QString &component : list) { version.append(component.toInt()); }
 }
 
-QString AutoUpdateChecker::getPlatform()
-{
+QString AutoUpdateChecker::getPlatform() {
 #if defined(STEAM_LINK)
     return QStringLiteral("steamlink");
 #elif defined(APP_IMAGE)
@@ -65,8 +53,7 @@ QString AutoUpdateChecker::getPlatform()
 #endif
 }
 
-void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply* reply)
-{
+void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply *reply) {
     Q_ASSERT(reply->isFinished());
 
     if (reply->error() == QNetworkReply::NoError) {
@@ -78,7 +65,7 @@ void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply* reply)
         reply->deleteLater();
 
         QJsonParseError error;
-        QJsonDocument jsonDoc = QJsonDocument::fromJson(jsonString.toUtf8(), &error);
+        QJsonDocument   jsonDoc = QJsonDocument::fromJson(jsonString.toUtf8(), &error);
         if (jsonDoc.isNull()) {
             qWarning() << "Update manifest malformed:" << error.errorString();
             return;
@@ -93,24 +80,17 @@ void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply* reply)
         for (QJsonValueRef updateEntry : array) {
             if (updateEntry.isObject()) {
                 QJsonObject updateObj = updateEntry.toObject();
-                if (!updateObj.contains("platform") ||
-                        !updateObj.contains("arch") ||
-                        !updateObj.contains("version") ||
-                        !updateObj.contains("browser_url")) {
+                if (!updateObj.contains("platform") || !updateObj.contains("arch") || !updateObj.contains("version") || !updateObj.contains("browser_url")) {
                     qWarning() << "Update manifest entry missing vital field";
                     continue;
                 }
 
-                if (!updateObj["platform"].isString() ||
-                        !updateObj["arch"].isString() ||
-                        !updateObj["version"].isString() ||
-                        !updateObj["browser_url"].isString()) {
+                if (!updateObj["platform"].isString() || !updateObj["arch"].isString() || !updateObj["version"].isString() || !updateObj["browser_url"].isString()) {
                     qWarning() << "Update manifest entry has unexpected vital field type";
                     continue;
                 }
 
-                if (updateObj["arch"] == QSysInfo::buildCpuArchitecture() &&
-                        updateObj["platform"] == getPlatform()) {
+                if (updateObj["arch"] == QSysInfo::buildCpuArchitecture() && updateObj["platform"] == getPlatform()) {
                     qDebug() << "Found update manifest match for current platform";
 
                     QString latestVersion = updateObj["version"].toString();
@@ -121,28 +101,20 @@ void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply* reply)
                     parseStringToVersionQuad(latestVersion, latestVersionQuad);
 
                     for (int i = 0;; i++) {
-                        int latestVer = 0;
+                        int latestVer  = 0;
                         int currentVer = 0;
 
                         // Treat missing decimal places as 0
-                        if (i < latestVersionQuad.count()) {
-                            latestVer = latestVersionQuad[i];
-                        }
-                        if (i < m_CurrentVersionQuad.count()) {
-                            currentVer = m_CurrentVersionQuad[i];
-                        }
-                        if (i >= latestVersionQuad.count() && i >= m_CurrentVersionQuad.count()) {
-                            break;
-                        }
+                        if (i < latestVersionQuad.count()) { latestVer = latestVersionQuad[i]; }
+                        if (i < m_CurrentVersionQuad.count()) { currentVer = m_CurrentVersionQuad[i]; }
+                        if (i >= latestVersionQuad.count() && i >= m_CurrentVersionQuad.count()) { break; }
 
                         if (currentVer < latestVer) {
                             qDebug() << "Update available";
 
-                            emit onUpdateAvailable(updateObj["version"].toString(),
-                                                   updateObj["browser_url"].toString());
+                            emit onUpdateAvailable(updateObj["version"].toString(), updateObj["browser_url"].toString());
                             return;
-                        }
-                        else if (currentVer > latestVer) {
+                        } else if (currentVer > latestVer) {
                             qDebug() << "Update manifest version lower than current version";
                             return;
                         }
@@ -152,16 +124,13 @@ void AutoUpdateChecker::handleUpdateCheckRequestFinished(QNetworkReply* reply)
 
                     return;
                 }
-            }
-            else {
+            } else {
                 qWarning() << "Update manifest contained unrecognized entry:" << updateEntry.toString();
             }
         }
 
-        qWarning() << "No entry in update manifest found for current platform: "
-                   << QSysInfo::buildCpuArchitecture() << getPlatform();
-    }
-    else {
+        qWarning() << "No entry in update manifest found for current platform: " << QSysInfo::buildCpuArchitecture() << getPlatform();
+    } else {
         qWarning() << "Update checking failed with error: " << reply->error();
         reply->deleteLater();
     }
